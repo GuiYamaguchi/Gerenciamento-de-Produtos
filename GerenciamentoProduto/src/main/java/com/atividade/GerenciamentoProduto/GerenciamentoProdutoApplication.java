ec0a3077f76110f2859ffd4dfaff678225751f7b
unknown <japajao25@gmail.com>
2026-03-19 10:33:09 -0300

    entidade criada

---
+++ GerenciamentoProduto/src/main/java/com/atividade/GerenciamentoProduto/GerenciamentoProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.GerenciamentoProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProdutoApplication.class, args);
+	}
+
+}
---
+++ GerenciamentoProduto/src/test/java/com/atividade/GerenciamentoProduto/GerenciamentoProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.GerenciamentoProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
