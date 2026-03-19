9336efae7531b6c0e2242c2f1b4953bcfb563f21
unknown <japajao25@gmail.com>
2026-03-19 10:34:23 -0300

    repository criado

---
+++ GerenciamentoProduto/src/main/java/com/atividade/GerenciamentoProduto/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.atividade.GerenciamentoProduto.repository;
+
+import com.atividade.GerenciamentoProduto.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
